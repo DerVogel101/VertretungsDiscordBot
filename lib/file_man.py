14bd6358ee90ee19ab45b5e696c32d5299818497
DerVogel101 <jan.irmer@outlook.de>
2023-09-07 10:23:41 +0200

    updated file_man.py

--- lib/file_man.py
+++ lib/file_man.py
@@ -2,29 +2,30 @@ import json
 from copy import deepcopy
 
 
-def load_json_dict(file: str, default_structure: list[tuple]) -> dict:
+def load_json_dict(file_name: str, default_structure: list[tuple]) -> dict:
     """
     Loads a json file to a dict. If the file is empty, it will be filled with the default_structure.
-    :param file: The file to load
+    :param file_name: The file to load
     :param default_structure: The default structure to fill the file with if it is empty (e.g. [("key", "value")]
     :return: The dict
     """
-    with open(file, 'r+') as f:
+    with open(file_name, 'r+') as file:
         try:
-            return json.load(f)
+            return json.load(file)
         except json.decoder.JSONDecodeError:
             default_structure_dict = {}
             for key, value in deepcopy(default_structure):
                 default_structure_dict[key] = value
-            json.dump(default_structure_dict, f)
+            json.dump(default_structure_dict, file)
+
             return default_structure_dict
 
 
-def save_json_dict(file: str, dict_to_save: dict):
+def save_json_dict(file_name: str, dict_to_save: dict) -> None:
     """
     Saves a dict to a json file.
-    :param file: The file to save
+    :param file_name: The file to save
     :param dict_to_save: The dict to save
     """
-    with open(file, 'w') as f:
-        json.dump(dict_to_save, f)
+    with open(file_name, 'w') as file:
+        json.dump(dict_to_save, file)
