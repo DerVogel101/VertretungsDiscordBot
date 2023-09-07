6bb6442edede1ce7838c3cf9a020374d83201745
DerVogel101 <jan.irmer@outlook.de>
2023-09-07 20:00:42 +0200

    merged functions into a class

--- VertBot.py
+++ VertBot.py
@@ -9,9 +9,10 @@ import signal
 import sys
 import os
 from pprint import pprint
+from copy import deepcopy
 
-sys.path.insert(1, "./lib")
-from file_man import load_json_dict, save_json_dict
+sys.path.insert(1, "./lib")  # Add lib folder to path
+
 
 
 def signal_handler(sig, frame):
@@ -47,54 +48,82 @@ if __name__ == "__main__":
 
     # Default structure for data.json
     # Structure:
-    # {"servers": {
-    #     server_id: {  # e.g. 123456789 type: int
-    #         "subjects": {
-    #             {subject_name: {  # e.g. "Mathe" type: str
-    #                 "teacher": str,  # teacher name
-    #                 "abscences": {
-    #                     {date: {  # e.g. "2021-09-01" type: str
-    #                         "reason": str,  # reason for abscence (e.g. "Krank")
-    #                         "is_late": bool,  # if the teacher is late
-    #                         "length": int,  # in minutes if is_late is True else in hours
-    #                         "is_replaced": bool,  # if a replacement teacher is set
-    #                         "replacement_present": bool,  # if the replacement teacher is present  or on "mit betreuung"
-    #                         "period": list[int, int],  # [start, end] in format [start period, end period]
-    #                         "note": str,  # optional note
-    #                         "reporter_id": int  # discord id of the reporter
-    #                     }}
-    #                 }
-    #             }}
-    #         },
-    #         "channel": int,  # channel id of the channel where the bot should send and recive the messages
-    #         "reporter_role": int  # role id of the reporter role
-    #     }
-    # }}
+    """NOTUSED = {"servers": {
+        server_id: {  # e.g. 123456789 type: int
+            "subjects": {
+                {subject_name: {  # e.g. "Mathe" type: str
+                    "teacher": str,  # teacher name
+                    "abscences": {
+                        {date: {  # e.g. "2021-09-01" type: str
+                            "reason": str,  # reason for abscence (e.g. "Krank")
+                            "is_late": bool,  # if the teacher is late
+                            "length": int,  # in minutes if is_late is True
+                            "is_replaced": bool,  # if a replacement teacher is set
+                            "replacement_present": bool,  # if the replacement teacher is present  or on "mit betreuung"
+                            "period": list[int, int],  # [start, end] in format [start period, end period]
+                            "note": str,  # optional note
+                            "reporter_id": int  # discord id of the reporter
+                        }}
+                    }
+                }}
+            },
+            "channel": int,  # channel id of the channel where the bot should send and recive the messages
+            "reporter_role": int  # role id of the reporter role
+        }
+    }}"""
 
 
+    class DiscordDatabaseApi:
+        class InvalidFileError(Exception):
+            pass
 
+        def __init__(self, file_name: str) -> None:
+            self.__file_name = file_name
+            self.__load()  # Load database from file into self.__database
 
-    default_database_structure = [
-        ("servers", {}),
-    ]
+        def __genrate_empty(self) -> None:
+            try:
+                with open(self.__file_name, "x") as f:
+                    json.dump({"servers": {}}, f)
+            except FileExistsError:
+                pass
 
-    # Function to load a json file or write an empty {} to it if its empty using a json.dump
-    database = load_json_dict("data.json", default_database_structure)
+        def __load(self) -> None:
+            for _ in range(2):
+                try:
+                    with open(self.__file_name, "r") as file:
+                        self.__database = json.load(file)
+                    break
+                except FileNotFoundError:
+                    self.__genrate_empty()
+                except json.decoder.JSONDecodeError:
+                    raise self.InvalidFileError("The file is not a valid json file")
+            else:
+                raise self.InvalidFileError("The file could not be generated")
 
-    # Function to save the database variable to data.json
-    def save_database() -> None:
-        """Saves the database variable to data.json"""
-        save_json_dict("data.json", database)
+        def __save(self) -> None:
+            with open(self.__file_name, "w") as file:
+                json.dump(self.__database, file)
 
+        def print(self) -> None:  # FIXME: Only for testing
+            pprint(self.__database)
 
-    def register_servers_database() -> None:  # Function to register a server in the database
-        """Registers all servers in the database"""
-        for guild in client.guilds:
-            if str(guild.id) not in database:
-                database[str(guild.id)] = {}
-                save_database()
-                print(f"Registered {guild.name} in database")
+        def get_database(self) -> dict:  # FIXME: Only for testing
+            self.__load()
+            return deepcopy(self.__database)
 
+        def register_servers(self, register_client: discord.client.Client) -> None:
+            """Registers all servers in the database"""
+            for guild in register_client.guilds:
+                if str(guild.id) not in self.__database["servers"]:
+                    self.__database["servers"][str(guild.id)] = {"subjects": {}, "channel": None, "reporter_role": None}
+                    self.__save()
+                    print(f"Registered {guild.name} in database")
+
+        # TODO: set_channel, set_subjects, set_channel, set_reporter_role, get_subjects, get_channel, get_reporter_role
+        ...
+
+    test = DiscordDatabaseApi("data.json")
 
     # Crash handling
     signal.signal(signal.SIGINT, signal_handler)
